b33c3c58ae24d01197f6e2b34d52559f7db8b13d
RudolphL <rudolphlesire@gmail.com>
2014-03-19 13:22:50 +0100

    Project

---
+++ Digital Circuit/Connection.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Digital_Circuit
+{
+    class Connection
+    {
+        // Field
+        private Boolean value;
+        private int startX, startY, endX, endY;
+        private Gate startgate;
+        private Gate endgate;
+        private static Brush[] myBrushes =
+        { new SolidBrush(Color.Green),
+          new SolidBrush(Color.Black),
+          new SolidBrush(Color.Red)  };
+        private Color connectioncolor;
+
+        // Constructor
+        public Connection(Boolean value, Gate startgate, Gate endgate)
+        {
+            //this.value = startgate.MyOutputValue;
+            this.startgate = startgate;
+            this.endgate = endgate;
+            this.connectioncolor = Color.Black;
+        }
+
+        // Properties
+
+        // Methods
+
+        /// <summary>
+        /// checks if the point (xmouse,ymouse) is on this connection.
+        /// </summary>
+        /// <param name="xmouse"></param>
+        /// <param name="ymouse"></param>
+        /// <returns></returns>
+        public bool ContainsPoint(int xmouse, int ymouse)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Draws a connection on it's location
+        /// </summary>
+        /// <param name="gr"></param>
+        public void DrawConnection(Graphics gr)
+        {
+            return;
+        }
+    }
+}
---
+++ Digital Circuit/GridObjects.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Digital_Circuit
+{
+    class GridObjects
+    {
+        // Fields
+        private List<Gate> gates;
+        private List<Connection> connections;
+
+        // Delegates and events
+
+        // Constructor
+        public GridObjects()
+        {
+            this.gates = new List<Gate>();
+            this.connections = new List<Connection>();
+        }
+
+        // Methods
+
+        /// <summary>
+        /// Returns a list of all gates
+        /// </summary>
+        /// <returns></returns>
+        public List<Gate> GetAllGates()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Returns a list of all connections
+        /// </summary>
+        /// <returns></returns>
+        public List<Connection> GetAllConnections()
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Looks for a gate in the list of gates that contains the point (xmouse,ymouse)
+        /// </summary>
+        /// <param name="xmouse"></param>
+        /// <param name="ymouse"></param>
+        /// <returns>Return gate if found, else return null</returns>
+        public Gate GetGate(int xmouse, int ymouse)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Looks for a connection in the list of connectoins that contains the point (xmouse,ymouse)
+        /// </summary>
+        /// <param name="xmouse"></param>
+        /// <param name="ymouse"></param>
+        /// <returns>returns connection if found, else null</returns>
+        public Connection GetConnection(int xmouse, int ymouse)
+        {
+            return null;
+        }
+
+        /// <summary>
+        /// Adds a gate to a specific location
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Return true if succesful</returns>
+        public bool AddGate(int x, int y)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Adds a connection between two gates
+        /// </summary>
+        /// <param name="xstart"></param>
+        /// <param name="ystart"></param>
+        /// <param name="xend"></param>
+        /// <param name="yend"></param>
+        /// <returns>Return true if succesful</returns>
+        public bool AddConnection(Gate startgate, Gate endgate)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Removes a gate from the list of gates.
+        /// If the gate has connections to it, these will be deleted too.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Returns true if succesful</returns>
+        public bool RemoveGate(Gate currentgate)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Removes a connection from the list of connections.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <returns>Returns true if succesful</returns>
+        public bool RemoveConnection(Connection currentconnection)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// Loads a circuit from a file.
+        /// </summary>
+        /// <param name="filename"></param>
+        public void LoadFile(string filename)
+        {
+            return;
+        }
+
+        /// <summary>
+        /// Save a circuit to a file.
+        /// </summary>
+        public void SaveFile()
+        {
+            return;
+        }
+
+        /// <summary>
+        /// Draws all the gates of the circuit
+        /// </summary>
+        /// <param name="gr"></param>
+        public void DrawAllGates(Graphics gr)
+        {
+            return;
+        }
+
+        /// <summary>
+        /// Draws all the connections of the circuit
+        /// Not sure wether to join this with the gates
+        /// </summary>
+        /// <param name="gr"></param>
+        public void DrawAllConnections(Graphics gr)
+        {
+            return;
+        }
+    }
+}
