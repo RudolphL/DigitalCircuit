f58c279c51f58149ff523aab95c32ffa27f98fa0
Fynze <vincentkeltjens@hotmail.com>
2014-04-03 12:52:11 +0200

    Added pictures to imagelist and project

--- Digital Circuit/FormDigitalCircuit.Designer.cs
+++ Digital Circuit/FormDigitalCircuit.Designer.cs
@@ -216,13 +216,13 @@
             // openToolStripMenuItem
             // 
             this.openToolStripMenuItem.Name = "openToolStripMenuItem";
-            this.openToolStripMenuItem.Size = new System.Drawing.Size(152, 32);
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(132, 32);
             this.openToolStripMenuItem.Text = "Open";
             // 
             // saveToolStripMenuItem
             // 
             this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
-            this.saveToolStripMenuItem.Size = new System.Drawing.Size(152, 32);
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(132, 32);
             this.saveToolStripMenuItem.Text = "Save";
             // 
             // toolStripSeparator1
@@ -256,9 +256,15 @@
             // 
             // imageList
             // 
-            this.imageList.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
-            this.imageList.ImageSize = new System.Drawing.Size(20, 20);
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
             this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "andgate.png");
+            this.imageList.Images.SetKeyName(1, "notgate.png");
+            this.imageList.Images.SetKeyName(2, "orgate.png");
+            this.imageList.Images.SetKeyName(3, "sinkoff.png");
+            this.imageList.Images.SetKeyName(4, "sinkon.png");
+            this.imageList.Images.SetKeyName(5, "sourceoff.png");
+            this.imageList.Images.SetKeyName(6, "sourceon.png");
             // 
             // FormDigitalCircuit
             // 
